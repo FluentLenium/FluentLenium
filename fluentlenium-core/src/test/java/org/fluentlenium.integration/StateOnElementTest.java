7dc019fcb27af4b37ba7d9c7753b7afaf9263617
Mathilde Lemee <mathilde.lemee@yahoo.fr>
2012-04-22 16:15:08 +0200

    optimize initialization

--- fluentlenium-core/src/test/java/org/fluentlenium.integration/StateOnElementTest.java
+++ fluentlenium-core/src/test/java/org/fluentlenium.integration/StateOnElementTest.java
@@ -15,45 +15,44 @@
 package org.fluentlenium.integration;
 
 import org.fluentlenium.integration.localtest.LocalFluentCase;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.fest.assertions.Assertions.assertThat;
 
 public class StateOnElementTest extends LocalFluentCase {
+    @Before
+    public void before() {
+        goTo(DEFAULT_URL);
+    }
 
     @Test
     public void checkIsEnabled() {
-        goTo(DEFAULT_URL);
         assertThat($("input").first().isEnabled()).isTrue();
     }
 
     @Test
     public void checkIsDisplayed() {
-        goTo(DEFAULT_URL);
         assertThat($("input").first().isDisplayed()).isTrue();
     }
 
     @Test
     public void checkIsNotSelected() {
-        goTo(DEFAULT_URL);
         assertThat($("input").first().isSelected()).isFalse();
     }
 
     @Test
     public void checkIsSelected() {
-        goTo(DEFAULT_URL);
         assertThat($("#selected").first().isSelected()).isTrue();
     }
 
     @Test
     public void checkIsDisabled() {
-        goTo(DEFAULT_URL);
         assertThat($("#disabled").first().isSelected()).isFalse();
     }
 
     @Test
     public void checkIsNonDisplay() {
-        goTo(DEFAULT_URL);
         assertThat($("#non_display").first().isDisplayed()).isFalse();
     }
 
