d2d79a008ee288c15d046e6162722f90f9e10034
jetoile <kmx.petals@gmail.com>
2013-05-03 00:06:36 +0200

    update licence

--- fluentlenium-cucumber/src/main/java/org/fluentlenium/cucumber/adapter/exception/UnsupportedDriverException.java
+++ fluentlenium-cucumber/src/main/java/org/fluentlenium/cucumber/adapter/exception/UnsupportedDriverException.java
@@ -1,12 +1,18 @@
+/**
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License
+ */
 package org.fluentlenium.cucumber.adapter.exception;
 
-/**
- * Created with IntelliJ IDEA.
- * User: khanh
- * Date: 30/04/13
- * Time: 08:05
- * To change this template use File | Settings | File Templates.
- */
 public class UnsupportedDriverException extends Exception {
     public UnsupportedDriverException(Object p0, Exception cause) {
     }
