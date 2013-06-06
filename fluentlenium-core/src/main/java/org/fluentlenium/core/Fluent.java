f9cbee57f85446a29041302e613e6d497a6e5b49
Mathilde Lemee <mathilde.lemee@yahoo.fr>
2013-06-06 15:28:28 +0200

    createPage is now available in Fluent and not in FluentAdapter

--- fluentlenium-core/src/main/java/org/fluentlenium/core/Fluent.java
+++ fluentlenium-core/src/main/java/org/fluentlenium/core/Fluent.java
@@ -17,16 +17,25 @@ package org.fluentlenium.core;
 import org.apache.commons.io.FileUtils;
 import org.fluentlenium.core.action.FillConstructor;
 import org.fluentlenium.core.action.FluentDefaultActions;
+import org.fluentlenium.core.annotation.AjaxElement;
+import org.fluentlenium.core.annotation.Page;
 import org.fluentlenium.core.domain.FluentList;
 import org.fluentlenium.core.domain.FluentWebElement;
+import org.fluentlenium.core.exception.ConstructionException;
 import org.fluentlenium.core.filter.Filter;
 import org.fluentlenium.core.search.Search;
 import org.fluentlenium.core.search.SearchActions;
 import org.fluentlenium.core.wait.FluentWait;
 import org.openqa.selenium.*;
+import org.openqa.selenium.support.pagefactory.AjaxElementLocatorFactory;
+import org.openqa.selenium.support.pagefactory.DefaultElementLocatorFactory;
+import org.openqa.selenium.support.pagefactory.ElementLocator;
+import org.openqa.selenium.support.pagefactory.ElementLocatorFactory;
+import org.openqa.selenium.support.pagefactory.internal.LocatingElementHandler;
 
 import java.io.File;
 import java.io.IOException;
+import java.lang.reflect.*;
 import java.net.URI;
 import java.util.Date;
 import java.util.List;
@@ -129,6 +138,103 @@ public abstract class Fluent implements SearchActions {
         return driver;
     }
 
+
+    protected void initTest() {
+        Class cls = null;
+        try {
+	    for (cls = Class.forName(this.getClass().getName()); FluentAdapter.class.isAssignableFrom(cls); cls = cls.getSuperclass()) {
+	        for (Field field : cls.getDeclaredFields()) {
+                if (field.isAnnotationPresent(Page.class)) {
+                    field.setAccessible(true);
+                    Class clsField = field.getType();
+                    Class clsPage = Class.forName(clsField.getName());
+                    Object page = initClass(clsPage);
+                    field.set(this, page);
+                }
+            }
+	    }
+        } catch (ClassNotFoundException e) {
+            throw new ConstructionException("Class " + (cls != null ? cls.getName() : " null") + "not found", e);
+        } catch (IllegalAccessException e) {
+            throw new ConstructionException("IllegalAccessException on class " + (cls != null ? cls.getName() : " null"), e);
+        }
+    }
+
+    public <T extends FluentPage> T createPage(Class<T> classOfPage) {
+        return initClass(classOfPage);
+    }
+
+
+    protected <T extends FluentPage> T initClass(Class<T> cls) {
+        T page = null;
+        try {
+            Constructor construct = cls.getDeclaredConstructor();
+            construct.setAccessible(true);
+            page = (T) construct.newInstance();
+            Class parent = Class.forName(Fluent.class.getName());
+            initDriver(page, parent);
+            initBaseUrl(page, parent);
+
+            //init fields with default proxies
+            Field[] fields = cls.getDeclaredFields();
+            for (Field fieldFromPage : fields) {
+                if (!FluentWebElement.class.isAssignableFrom(fieldFromPage.getType())) {
+                    continue;
+                }
+                fieldFromPage.setAccessible(true);
+                AjaxElement elem = fieldFromPage.getAnnotation(AjaxElement.class);
+                if (elem == null) {
+                    proxyElement(new DefaultElementLocatorFactory(getDriver()), page, fieldFromPage);
+                } else {
+                    proxyElement(new AjaxElementLocatorFactory(getDriver(), elem.timeOutInSeconds()), page, fieldFromPage);
+                }
+            }
+        } catch (ClassNotFoundException e) {
+            throw new ConstructionException("Class " + (cls != null ? cls.getName() : " null") + "not found", e);
+        } catch (IllegalAccessException e) {
+            throw new ConstructionException("IllegalAccessException on class " + (cls != null ? cls.getName() : " null"), e);
+        } catch (NoSuchMethodException e) {
+            throw new ConstructionException("No constructor found on class " + (cls != null ? cls.getName() : " null"), e);
+        } catch (InstantiationException e) {
+            throw new ConstructionException("Unable to instantiate " + (cls != null ? cls.getName() : " null"), e);
+        } catch (InvocationTargetException e) {
+            throw new ConstructionException("Cannot invoke method setDriver on " + (cls != null ? cls.getName() : " null"), e);
+        }
+        return page;
+    }
+
+    private <T extends FluentPage> void initBaseUrl(T page, Class parent) throws NoSuchMethodException, IllegalAccessException, InvocationTargetException {
+        Method m;
+        if (getBaseUrl() != null) {
+            m = parent.getDeclaredMethod("withDefaultUrl", String.class);
+            m.setAccessible(true);
+            m.invoke(page, getBaseUrl());
+        }
+    }
+
+    private <T extends FluentPage> void initDriver(T page, Class parent) throws NoSuchMethodException, IllegalAccessException, InvocationTargetException {
+        Method m = parent.getDeclaredMethod("initFluent", WebDriver.class);
+        m.setAccessible(true);
+        m.invoke(page, getDriver());
+
+    }
+
+    private static void proxyElement(ElementLocatorFactory factory, Object page, Field field) {
+        ElementLocator locator = factory.createLocator(field);
+        if (locator == null) {
+            return;
+        }
+
+        InvocationHandler handler = new LocatingElementHandler(locator);
+        WebElement proxy = (WebElement) java.lang.reflect.Proxy.newProxyInstance(
+                page.getClass().getClassLoader(), new Class[]{WebElement.class}, handler);
+        try {
+            field.setAccessible(true);
+            field.set(page, new FluentWebElement(proxy));
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
     /**
      * Get the base URL to use when visiting relative URLs, if one is configured
      *
