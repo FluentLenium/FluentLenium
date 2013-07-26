01ebfe2d30feab3ef276fa3b8af6f4b8b091ae98
romainlouvet <mobileagri@gmail.com>
2013-07-26 10:38:07 +0200

    try to add test for alert and max

--- fluentlenium-core/src/test/java/org/fluentlenium/integration/AlertTest.java
+++ fluentlenium-core/src/test/java/org/fluentlenium/integration/AlertTest.java
@@ -1,17 +1,14 @@
 /**
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *         http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the
+ * License at http://www.apache.org/licenses/LICENSE-2.0 Unless required by applicable law or agreed to in writing, software distributed under the License is
+ * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing
+ * permissions and limitations under the License
  */
 package org.fluentlenium.integration;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+import java.util.concurrent.TimeUnit;
 
 import org.fluentlenium.core.FluentPage;
 import org.fluentlenium.core.annotation.Page;
@@ -21,21 +18,19 @@ import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-import static org.fest.assertions.Assertions.assertThat;
-
 public class AlertTest extends SauceLabsFluentCase {
 
   @Page
   private AlertPage alertPage;
 
-  /**
-   * Need real driver for test alert in DOM
-   *
-   * @return
-   */
   @Override
   public WebDriver getDefaultDriver() {
     return new FirefoxDriver();
+  }
+
+  @Override
+  public void setDefaultConfig() {
+    withDefaultSearchWait(5, TimeUnit.HOURS);
   }
 
   @Test
@@ -93,7 +88,6 @@ public class AlertTest extends SauceLabsFluentCase {
     assertThat($("#result").getText()).isEqualTo("it works");
 
   }
-
 
 }
 
