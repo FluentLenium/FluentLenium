e90174251b3b57d987124d5cf1cd9a92e64a8d7a
Pierre Leresteux <pierre.leresteux@gmail.com>
2014-04-20 22:10:57 +0200

    #114 : Remove Jetty Using static URL for testNG module

--- fluentlenium-testng/src/test/java/org/fluentlenium/integration/localtest/LocalFluentCase.java
+++ fluentlenium-testng/src/test/java/org/fluentlenium/integration/localtest/LocalFluentCase.java
@@ -19,11 +19,20 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 
 public abstract class LocalFluentCase extends FluentTestNg {
-    public static final String DEFAULT_URL = "http://localhost:8787/static/";
+    private static final String BASE_URL = getPath();
+    public static final String DEFAULT_URL = BASE_URL+"index.html";
 
     @Override
     public WebDriver getDefaultDriver() {
         return new HtmlUnitDriver(true);
+    }
+
+    public final static String getPath() {
+        String currentDir = System.getProperty("user.dir");
+        if (!currentDir.endsWith("/fluentlenium-core")){
+            currentDir+="/fluentlenium-core";
+        }
+        return "file:/" + currentDir + "/src/test/html/";
     }
 }
 
