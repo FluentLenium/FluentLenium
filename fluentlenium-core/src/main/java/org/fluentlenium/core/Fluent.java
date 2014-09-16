a368c6a695ea2422b329586bd86f4dc3984618d5
Ahmed Elmehri <ahmed.elmehri@gmail.com>
2014-09-16 10:47:24 +0100

    Add field in Fluent class that holds the result of #executeScript(String) method.

--- fluentlenium-core/src/main/java/org/fluentlenium/core/Fluent.java
+++ fluentlenium-core/src/main/java/org/fluentlenium/core/Fluent.java
@@ -50,6 +50,7 @@ public abstract class Fluent implements SearchActions {
     private WebDriver driver;
     private String baseUrl;
     private Search search;
+	private Object jsExecResult;
 
     public Fluent(WebDriver driver) {
         this.driver = driver;
@@ -246,6 +247,25 @@ public abstract class Fluent implements SearchActions {
         return baseUrl;
     }
 
+	/**
+	 * Get the result of JS script execution that occurred in
+	 * {@link #executeScript(String)} method.
+	 *
+	 * @return the JS script execution result, or null in the following
+	 * cases:
+	 * <ul>
+	 *     <li>
+	 *         the {@link #executeScript(String)} wasn't called, otherwise if
+	 *     </li>
+	 *     <li>the result value is null, or</li>
+	 *     <li>there is no result to return.</li>
+	 * </ul>
+	 */
+	public Object getJsExecResult()
+	{
+		return jsExecResult;
+	}
+
     /**
      * wait for an asynchronous call
      *
@@ -339,7 +359,7 @@ public abstract class Fluent implements SearchActions {
 
 
     public Fluent executeScript(String script) {
-        ((JavascriptExecutor) driver).executeScript(script);
+        jsExecResult = ((JavascriptExecutor) driver).executeScript(script);
         return this;
     }
 
