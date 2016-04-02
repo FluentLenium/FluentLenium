95338f32dde37865a67090dae628a517d9de3fe7
filipcynarski <filip@cynarski.eu>
2016-04-02 22:00:21 +0200

    resolves #231, PageUrl annotation added within tests - java doc updated

--- fluentlenium-core/src/main/java/org/fluentlenium/core/annotation/PageUrl.java
+++ fluentlenium-core/src/main/java/org/fluentlenium/core/annotation/PageUrl.java
@@ -4,11 +4,19 @@ import java.lang.annotation.Retention;
 
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
+/**
+ * <b>PageUrl</b> is a class annotation used instead of <b>getUrl</b> method of <b>FluentPage</b> object.
+ * If <b>PageUrl</b> annotation is used the page class may not override the <b>getUrl</b> method.
+ */
 @Retention(RUNTIME)
 public @interface PageUrl {
     /**
-     * The page URL.
-     * URLs starting with {@code "/"} (slash), are treated as relative pages
+     * The page URL can be relative or absolute, if the URL is not recognized
+     * as absolute will be treated as relative.
+     * <p/>
+     * For example :
+     * <code>@PageUrl("/index.html")</code>        should redirect to baseUrl + "/index.html"
+     * <code>@PageUrl("http://example.com")</code> should redirect to "http://example.com"
      *
      * @return page url
      */
