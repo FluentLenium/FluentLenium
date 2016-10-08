359f3b792ff74a50bb53b2e63e12563707777c4e
Toilal <toilal.dev@gmail.com>
2016-10-08 15:08:56 +0200

    Make tests run in parralel

--- fluentlenium-core/src/test/java/org/fluentlenium/integration/SharedDriverTestSuite.java
+++ fluentlenium-core/src/test/java/org/fluentlenium/integration/SharedDriverTestSuite.java
@@ -1,5 +1,6 @@
 package org.fluentlenium.integration;
 
+import net.jcip.annotations.NotThreadSafe;
 import org.fluentlenium.integration.ignore.SharedDriverDeleteCookies;
 import org.fluentlenium.integration.ignore.SharedDriverOnce1;
 import org.fluentlenium.integration.ignore.SharedDriverOnce2;
@@ -11,8 +12,9 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses(value = { SharedDriverDeleteCookies.class, SharedDriverOnce1.class, SharedDriverOnce2.class,
+@Suite.SuiteClasses(value = {SharedDriverDeleteCookies.class, SharedDriverOnce1.class, SharedDriverOnce2.class,
         SharedDriverPerClass1.class, SharedDriverPerClass2.class, SharedDriverPerMethodByAnnotation.class,
-        SharedDriverPerMethodByDefault.class })
-public class SharedDriverSuiteTest {
+        SharedDriverPerMethodByDefault.class})
+@NotThreadSafe
+public class SharedDriverTestSuite {
 }
--- fluentlenium-cucumber/src/test/java/org/fluentlenium/adapter/cucumber/integration/BasicRunner.java
+++ fluentlenium-cucumber/src/test/java/org/fluentlenium/adapter/cucumber/integration/BasicRunner.java
@@ -2,11 +2,13 @@ package org.fluentlenium.adapter.cucumber.integration;
 
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
+import net.jcip.annotations.NotThreadSafe;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "classpath:org/fluentlenium/adapter/cucumber/integration", format = { "pretty",
-        "html:target/cucumber", "json:target/cucumber.json" })
+@CucumberOptions(features = "classpath:org/fluentlenium/adapter/cucumber/integration", format = {"pretty", "html:target/cucumber",
+        "json:target/cucumber.json"})
+@NotThreadSafe
 public class BasicRunner {
 
 }
