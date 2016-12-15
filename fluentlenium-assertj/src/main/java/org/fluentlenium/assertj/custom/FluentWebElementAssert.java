70db9c9d59b85d9706e33801325598253a5284a5
Christoph Kutzinski <ckutz@esailors.de>
2016-12-15 11:28:08 +0000

    #440: prevent race conditions in the assertion error message

--- fluentlenium-assertj/src/main/java/org/fluentlenium/assertj/custom/FluentWebElementAssert.java
+++ fluentlenium-assertj/src/main/java/org/fluentlenium/assertj/custom/FluentWebElementAssert.java
@@ -49,6 +49,7 @@ public class FluentWebElementAssert extends AbstractAssert<FluentWebElementAsser
         if (actual.enabled()) {
             failIsNotEnabled();
         }
+
         return this;
     }
 
@@ -126,8 +127,9 @@ public class FluentWebElementAssert extends AbstractAssert<FluentWebElementAsser
      * @return {@code this} assertion object.
      */
     public FluentWebElementAssert hasText(final String textToFind) {
-        if (!actual.text().contains(textToFind)) {
-            failWithMessage("The element does not contain the text: " + textToFind + " . Actual text found : " + actual.text());
+        final String actualText = actual.text();
+        if (!actualText.contains(textToFind)) {
+            failWithMessage("The element does not contain the text: " + textToFind + " . Actual text found : " + actualText);
         }
 
         return this;
@@ -140,10 +142,10 @@ public class FluentWebElementAssert extends AbstractAssert<FluentWebElementAsser
      * @return {@code this} assertion object.
      */
     public FluentWebElementAssert hasTextMatching(final String regexToBeMatched) {
-        if (!actual.text().matches(regexToBeMatched)) {
+        final String actualText = actual.text();
+        if (!actualText.matches(regexToBeMatched)) {
             failWithMessage(
-                    "The element does not match the regex: " + regexToBeMatched + " . Actual text found : " + actual.text());
-
+                    "The element does not match the regex: " + regexToBeMatched + " . Actual text found : " + actualText);
         }
 
         return this;
@@ -178,8 +180,9 @@ public class FluentWebElementAssert extends AbstractAssert<FluentWebElementAsser
      * @return {@code this} assertion object.
      */
     public FluentWebElementAssert hasId(final String idToFind) {
-        if (!actual.id().equals(idToFind)) {
-            failWithMessage("The element does not have the id: " + idToFind + " . Actual id found : " + actual.id());
+        final String actualId = actual.id();
+        if (!actualId.equals(idToFind)) {
+            failWithMessage("The element does not have the id: " + idToFind + " . Actual id found : " + actualId);
         }
         return this;
     }
@@ -191,15 +194,15 @@ public class FluentWebElementAssert extends AbstractAssert<FluentWebElementAsser
      * @return {@code this} assertion object.
      */
     public FluentWebElementAssert hasClass(final String classToFind) {
-        if (!getClasses().contains(classToFind)) {
-            failWithMessage("The element does not have the class: " + classToFind + " . Actual class found : " + actual
-                    .attribute("class"));
+        final String actualClasses = actual.attribute("class");
+        if (!getClasses(actualClasses).contains(classToFind)) {
+            failWithMessage("The element does not have the class: " + classToFind + " . Actual class found : " + actualClasses);
         }
         return this;
     }
 
-    private List<String> getClasses() {
-        final String[] primitiveList = actual.attribute("class").split(" ");
+    private List<String> getClasses(final String classString) {
+        final String[] primitiveList = classString.split(" ");
         return Arrays.asList(primitiveList);
     }
 }
