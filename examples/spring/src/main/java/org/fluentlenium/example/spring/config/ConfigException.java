de9b50385b5be13c711743d18d626ecadabe8457
Toilal <toilal.dev@gmail.com>
2016-12-17 23:53:03 +0100

    Update docs and examples to next release version

--- examples/spring/src/main/java/org/fluentlenium/example/spring/config/ConfigException.java
+++ examples/spring/src/main/java/org/fluentlenium/example/spring/config/ConfigException.java
@@ -9,7 +9,7 @@ public class ConfigException extends RuntimeException {
      * Creates configuration exception
      *
      * @param message message
-     * @param cause cause
+     * @param cause   cause
      */
     public ConfigException(String message, Exception cause) {
         super(message, cause);
