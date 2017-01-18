675b0c1293ea192eccae4b7e3354f8f8df0828dd
Jarle Hansen <jarle@jarlehansen.net>
2017-01-18 18:15:15 +0100

    added groovy delegate for the FluentSpecificationAdapter

--- fluentlenium-spock/src/main/groovy/org/fluentlenium/adapter/spock/FluentSpecification.groovy
+++ fluentlenium-spock/src/main/groovy/org/fluentlenium/adapter/spock/FluentSpecification.groovy
@@ -2,35 +2,57 @@ package org.fluentlenium.adapter.spock
 
 import org.fluentlenium.adapter.FluentTestRunnerAdapter
 import org.fluentlenium.adapter.junit.FluentTestRule
+import org.fluentlenium.core.domain.FluentList
+import org.fluentlenium.core.domain.FluentWebElement
+import org.fluentlenium.core.search.SearchFilter
 import org.junit.ClassRule
 import org.junit.Rule
 import org.junit.rules.TestRule
 import org.junit.runner.Description
 import org.junit.runners.model.Statement
+import org.openqa.selenium.By
+import org.openqa.selenium.WebElement
 import spock.lang.Specification
 
 class FluentSpecification extends Specification {
 
-    protected FluentSpecificationAdapter fl = new FluentSpecificationAdapter()
+    @Delegate
+    private FluentSpecificationAdapter fl = new FluentSpecificationAdapter()
+
+    FluentList<FluentWebElement> $(List<WebElement> rawElements) {
+        return fl.$(rawElements)
+    }
+
+    FluentList<FluentWebElement> $(By locator, SearchFilter... filters) {
+        return fl.$(locator, filters)
+    }
+
+    FluentList<FluentWebElement> $(String selector, SearchFilter... filters) {
+        return fl.$(selector, filters)
+    }
+
+    FluentList<FluentWebElement> $(SearchFilter... filters) {
+        return fl.$(filters)
+    }
 
     @Rule
     public TestRule watchman = new FluentTestRule(this) {
         @Override
         void starting(Description description) {
             super.starting(description)
-            fl.specStarting(description.getTestClass(), description.getDisplayName())
+            specStarting(description.getTestClass(), description.getDisplayName())
         }
 
         @Override
         void finished(Description description) {
             super.finished(description)
-            fl.specFinished(description.getTestClass(), description.getDisplayName())
+            specFinished(description.getTestClass(), description.getDisplayName())
         }
 
         @Override
         void failed(Throwable e, Description description) {
             super.failed(e, description)
-            fl.specFailed(e, description.getTestClass(), description.getDisplayName())
+            specFailed(e, description.getTestClass(), description.getDisplayName())
         }
     }
 
