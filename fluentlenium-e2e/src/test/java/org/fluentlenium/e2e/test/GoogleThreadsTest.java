5cc8e76317ceb4668d94902b0d272a92a599b9a5
filipcynarski <filip@cynarski.eu>
2017-07-10 14:42:20 +0200

    FluentLenium 3.3.0 snapshot prepare

---
+++ fluentlenium-e2e/src/test/java/org/fluentlenium/e2e/test/GoogleThreadsTest.java
@@ -0,0 +1,24 @@
+package org.fluentlenium.e2e.test;
+
+import java.util.concurrent.TimeUnit;
+
+import org.assertj.core.api.Assertions;
+import org.fluentlenium.configuration.ConfigurationProperties;
+import org.fluentlenium.configuration.FluentConfiguration;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+
+@FluentConfiguration(driverLifecycle = ConfigurationProperties.DriverLifecycle.THREAD)
+public class GoogleThreadsTest extends E2ETest {
+
+    @Test(invocationCount = 5, threadPoolSize = 5)
+    public void firstMethod() {
+        goTo("http://www.google.com");
+        await().atMost(1, TimeUnit.SECONDS).until($(".gsfi")).present();
+    }
+
+    @AfterMethod
+    public void afterMethod() {
+        Assertions.assertThat(window().title()).isEqualTo("Google");
+    }
+}
