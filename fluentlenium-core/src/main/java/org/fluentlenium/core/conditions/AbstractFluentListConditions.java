a8fd429930f7430bad58aa5404532b3abd8c1d03
filipcynarski <filip@cynarski.eu>
2017-10-18 13:30:39 +0200

    Dynamic list check

--- fluentlenium-core/src/main/java/org/fluentlenium/core/conditions/AbstractFluentListConditions.java
+++ fluentlenium-core/src/main/java/org/fluentlenium/core/conditions/AbstractFluentListConditions.java
@@ -74,7 +74,7 @@ public abstract class AbstractFluentListConditions implements FluentListConditio
 
     @Override
     public boolean present() {
-        return verify(input -> input.conditions().present(), false);
+        return new DynamicListConditionsImpl(() -> elements, false).present();
     }
 
     @Override
@@ -89,7 +89,7 @@ public abstract class AbstractFluentListConditions implements FluentListConditio
 
     @Override
     public boolean displayed() {
-        return verify(input -> input.conditions().displayed(), false);
+        return new DynamicListConditionsImpl(() -> elements, false).displayed();
     }
 
     @Override
