17646f3ecbf646288edc476b0e42f5b4fe8defbc
filipcynarski <filip@cynarski.eu>
2018-03-12 09:30:21 +0100

    Prepare JUnit 5 updates

---
+++ fluentlenium-junit5/src/it/junit5/junit-parallel-classes/src/test/java/org/fluentlenium/it/Test2.java
@@ -0,0 +1,47 @@
+package org.fluentlenium.it;
+
+import org.junit5.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class Test2 extends FluentIntegTest {
+    @Test
+    public void test5() {
+        goTo(UrlUtil.getAbsoluteUrlFromFile("inputs.html"));
+        el("input").fill().with("5");
+
+        await().explicitlyFor(5000L);
+
+        assertThat(el("input").value()).isEqualTo("5");
+    }
+
+    @Test
+    public void test6() {
+        goTo(UrlUtil.getAbsoluteUrlFromFile("inputs.html"));
+        el("input").fill().with("6");
+
+        await().explicitlyFor(4000L);
+
+        assertThat(el("input").value()).isEqualTo("6");
+    }
+
+    @Test
+    public void test7() {
+        goTo(UrlUtil.getAbsoluteUrlFromFile("inputs.html"));
+        el("input").fill().with("7");
+
+        await().explicitlyFor(3000L);
+
+        assertThat(el("input").value()).isEqualTo("7");
+    }
+
+    @Test
+    public void test8() {
+        goTo(UrlUtil.getAbsoluteUrlFromFile("inputs.html"));
+        el("input").fill().with("8");
+
+        await().explicitlyFor(2000L);
+
+        assertThat(el("input").value()).isEqualTo("8");
+    }
+}
