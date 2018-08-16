290573850f8af184bf89686293f1577dfa4c4efb
Adrian Koryl <adrian.koryl@gmail.com>
2018-08-16 09:07:27 +0200

    Changed Cucumber version to 3.0.2

--- examples/cucumber/src/test/java/org/fluentlenium/examples/quickstart/BasicTest.java
+++ examples/cucumber/src/test/java/org/fluentlenium/examples/quickstart/BasicTest.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "classpath:org/fluentlenium/examples/quickstart",
-        format = {"pretty", "html:target/cucumber", "json:target/cucumber.json"})
+        plugin = {"pretty", "html:target/cucumber", "json:target/cucumber.json"})
 public class BasicTest {
 
 }
