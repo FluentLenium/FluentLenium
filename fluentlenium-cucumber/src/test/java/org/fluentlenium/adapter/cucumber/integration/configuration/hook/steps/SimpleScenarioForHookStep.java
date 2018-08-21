22523f0f3d5d870bbffe556e6672e9a015bd0e0a
Adrian Koryl <adrian.koryl@gmail.com>
2018-08-21 13:14:25 +0200

    Deleted unnecessary test

--- fluentlenium-cucumber/src/test/java/org/fluentlenium/adapter/cucumber/integration/configuration/hook/steps/SimpleScenarioForHookStep.java
+++ fluentlenium-cucumber/src/test/java/org/fluentlenium/adapter/cucumber/integration/configuration/hook/steps/SimpleScenarioForHookStep.java
@@ -15,7 +15,7 @@ public class SimpleScenarioForHookStep {
     @Page
     private LocalWithHookPage2 page2;
 
-    @Given(value = "scenario I am on the first page")
+    @Given(value = "scenario I am on the first Wait hook page")
     public void step1() {
         page.go();
     }
@@ -25,7 +25,7 @@ public class SimpleScenarioForHookStep {
         page.clickLink();
     }
 
-    @Then(value = "scenario I am on the second page")
+    @Then(value = "scenario I am on the second Wait hook page")
     public void step3() {
         page2.isAt();
     }
--- fluentlenium-cucumber/src/test/resources/org/fluentlenium/adapter/cucumber/integration/configuration/hook/basicScenarioForHook.feature
+++ fluentlenium-cucumber/src/test/resources/org/fluentlenium/adapter/cucumber/integration/configuration/hook/basicScenarioForHook.feature
@@ -4,8 +4,3 @@ Feature: basic test to check Wait hook
     Given scenario I am on the first Wait hook page
     When scenario I click on next page
     Then scenario I am on the second Wait hook page
-
-  Scenario: scenario 2
-    Given scenario I am on the first Wait hook page
-    When scenario I click on next page
-    Then scenario I am on the second Wait hook page
