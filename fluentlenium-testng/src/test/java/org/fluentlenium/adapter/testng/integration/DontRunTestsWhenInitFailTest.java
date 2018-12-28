6064b20e508b73b31f0f28e9d36362cd1fc9a867
Tamas Balog <picimako@gmail.com>
2018-12-28 19:20:40 +0100

    #599 - Call recommended addListener method of TestNG

--- fluentlenium-testng/src/test/java/org/fluentlenium/adapter/testng/integration/DontRunTestsWhenInitFailTest.java
+++ fluentlenium-testng/src/test/java/org/fluentlenium/adapter/testng/integration/DontRunTestsWhenInitFailTest.java
@@ -3,15 +3,17 @@ package org.fluentlenium.adapter.testng.integration;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
-import org.fluentlenium.adapter.testng.FluentTestNg;
 import org.mockito.Mockito;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 import org.testng.Assert;
+import org.testng.ITestNGListener;
 import org.testng.ITestResult;
 import org.testng.TestListenerAdapter;
 import org.testng.TestNG;
 import org.testng.annotations.Test;
+
+import org.fluentlenium.adapter.testng.FluentTestNg;
 
 public class DontRunTestsWhenInitFailTest {
 
@@ -37,19 +39,16 @@ public class DontRunTestsWhenInitFailTest {
 
     @Test
     public void testRun() {
-
         TestNG testNG = new TestNG(false);
         testNG.setTestClasses(new Class[] {TestClass.class});
 
         TestListenerAdapter listenerAdapter = Mockito.mock(TestListenerAdapter.class);
-        testNG.addListener(listenerAdapter);
+        testNG.addListener((ITestNGListener) listenerAdapter);
 
         testNG.run();
 
-        verify(listenerAdapter, times(2))
-                .onConfigurationFailure(Mockito.any(ITestResult.class));
+        verify(listenerAdapter, times(2)).onConfigurationFailure(Mockito.any(ITestResult.class));
         verify(listenerAdapter).onTestSkipped(Mockito.any(ITestResult.class));
         verify(listenerAdapter, Mockito.never()).onTestSuccess(Mockito.any(ITestResult.class));
     }
-
 }
