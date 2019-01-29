2d0d07146d289f9b18753bbd616cf6fb32ee3647
filip.cynarski <filip@cynarski.eu>
2019-01-29 17:19:33 +0100

    Fixes for 4.1.0 release

--- fluentlenium-core/src/main/java/org/fluentlenium/core/domain/FluentWebElement.java
+++ fluentlenium-core/src/main/java/org/fluentlenium/core/domain/FluentWebElement.java
@@ -1,38 +1,36 @@
 package org.fluentlenium.core.domain;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
-import java.util.Stack;
+import java.util.*;
 import java.util.function.Function;
+
+import org.fluentlenium.configuration.ConfigurationFactory;
+import org.fluentlenium.configuration.ConfigurationProperties;
 import org.fluentlenium.core.FluentControl;
-import org.fluentlenium.core.action.Fill;
-import org.fluentlenium.core.action.FillSelect;
-import org.fluentlenium.core.action.FluentActions;
-import org.fluentlenium.core.action.FluentJavascriptActionsImpl;
-import org.fluentlenium.core.action.KeyboardElementActions;
-import org.fluentlenium.core.action.MouseElementActions;
+import org.fluentlenium.core.FluentPage;
+import org.fluentlenium.core.action.*;
 import org.fluentlenium.core.components.ComponentInstantiator;
 import org.fluentlenium.core.conditions.FluentConditions;
 import org.fluentlenium.core.conditions.WebElementConditions;
+import org.fluentlenium.core.css.CssSupport;
 import org.fluentlenium.core.dom.Dom;
+import org.fluentlenium.core.events.EventsRegistry;
 import org.fluentlenium.core.hook.FluentHook;
 import org.fluentlenium.core.hook.HookControl;
 import org.fluentlenium.core.hook.HookControlImpl;
 import org.fluentlenium.core.hook.HookDefinition;
+import org.fluentlenium.core.inject.ContainerContext;
 import org.fluentlenium.core.label.FluentLabel;
 import org.fluentlenium.core.label.FluentLabelImpl;
 import org.fluentlenium.core.proxy.FluentProxyState;
 import org.fluentlenium.core.proxy.LocatorHandler;
 import org.fluentlenium.core.proxy.LocatorProxies;
+import org.fluentlenium.core.script.FluentJavascript;
 import org.fluentlenium.core.search.Search;
 import org.fluentlenium.core.search.SearchControl;
 import org.fluentlenium.core.search.SearchFilter;
 import org.fluentlenium.core.wait.FluentWaitElement;
 import org.fluentlenium.utils.SupplierOfInstance;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Dimension;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.internal.WrapsElement;
 import org.openqa.selenium.support.pagefactory.ElementLocator;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -74,7 +72,7 @@ public class FluentWebElement extends Component
                     LocatorHandler locatorHandler = LocatorProxies.getLocatorHandler(getElement());
                     ElementLocator locator = locatorHandler.getLocator();
                     WebElement noHookElement = LocatorProxies.createWebElement(locator);
-                    return getFluentControl().newComponent(this.getClass(), noHookElement);
+                    return control.newComponent(this.getClass(), noHookElement);
                 });
 
         search = new Search(element, this, this.instantiator, this.control);
@@ -84,10 +82,6 @@ public class FluentWebElement extends Component
         conditions = new WebElementConditions(this);
         label = new FluentLabelImpl<>(this, () -> getElement().toString());
         javascriptActions = new FluentJavascriptActionsImpl<>(this, this.control, new SupplierOfInstance<>(this));
-    }
-
-    private FluentControl getFluentControl() { // NOPMD UnusedPrivateMethod
-        return control;
     }
 
     private HookControl<FluentWebElement> getHookControl() { // NOPMD UnusedPrivateMethod
@@ -100,6 +94,392 @@ public class FluentWebElement extends Component
 
     public FluentLabel<FluentWebElement> getLabel() {
         return label;
+    }
+
+
+    public FluentJavascript executeScript(String script, Object... args) {
+        return control.executeScript(script, args);
+    }
+
+    public FluentJavascript executeAsyncScript(String script, Object... args) {
+        return control.executeAsyncScript(script, args);
+    }
+
+    public Alert alert() {
+        return control.alert();
+    }
+
+    public void takeHtmlDump() {
+        control.takeHtmlDump();
+    }
+
+    public void takeHtmlDump(String fileName) {
+        control.takeHtmlDump(fileName);
+    }
+
+    public boolean canTakeScreenShot() {
+        return control.canTakeScreenShot();
+    }
+
+    public void takeScreenshot() {
+        control.takeScreenshot();
+    }
+
+    public void takeScreenshot(String fileName) {
+        control.takeScreenshot(fileName);
+    }
+
+    public EventsRegistry events() {
+        return control.events();
+    }
+
+    public <P extends FluentPage> P goTo(P page) {
+        return control.goTo(page);
+    }
+
+    public void goTo(String url) {
+        control.goTo(url);
+    }
+
+    public void goToInNewTab(String url) {
+        control.goToInNewTab(url);
+    }
+
+    public void switchTo(FluentList<? extends FluentWebElement> elements) {
+        control.switchTo(elements);
+    }
+
+    public void switchTo(FluentWebElement element) {
+        control.switchTo(element);
+    }
+
+    public void switchTo() {
+        control.switchTo();
+    }
+
+    public void switchToDefault() {
+        control.switchToDefault();
+    }
+
+    public String pageSource() {
+        return control.pageSource();
+    }
+
+    public WindowAction window() {
+        return control.window();
+    }
+
+    public Set<Cookie> getCookies() {
+        return control.getCookies();
+    }
+
+    public Cookie getCookie(String name) {
+        return control.getCookie(name);
+    }
+
+    public String url() {
+        return control.url();
+    }
+
+    public WebDriver getDriver() {
+        return control.getDriver();
+    }
+
+    public CssSupport css() {
+        return control.css();
+    }
+
+    public ContainerContext inject(Object container) {
+        return control.inject(container);
+    }
+
+    public ContainerContext injectComponent(Object componentContainer, Object parentContainer, SearchContext context) {
+        return control.injectComponent(componentContainer, parentContainer, context);
+    }
+
+    public <T> T newInstance(Class<T> cls) {
+        return control.newInstance(cls);
+    }
+
+
+    public FluentWebElement newFluent(WebElement element) {
+        return control.newFluent(element);
+    }
+
+    public <T> T newComponent(Class<T> componentClass, WebElement element) {
+        return control.newComponent(componentClass, element);
+    }
+
+    public FluentList<FluentWebElement> newFluentList() {
+        return control.newFluentList();
+    }
+
+    public FluentList<FluentWebElement> newFluentList(FluentWebElement... elements) {
+        return control.newFluentList(elements);
+    }
+
+    public FluentList<FluentWebElement> newFluentList(List<FluentWebElement> elements) {
+        return control.newFluentList(elements);
+    }
+
+    public FluentList<FluentWebElement> asFluentList(WebElement... elements) {
+        return control.asFluentList(elements);
+    }
+
+    public FluentList<FluentWebElement> asFluentList(Iterable<WebElement> elements) {
+        return control.asFluentList(elements);
+    }
+
+    public FluentList<FluentWebElement> asFluentList(List<WebElement> elements) {
+        return control.asFluentList(elements);
+    }
+
+    public <T extends FluentWebElement> FluentList<T> newFluentList(Class<T> componentClass) {
+        return control.newFluentList(componentClass);
+    }
+
+    public <T extends FluentWebElement> FluentList<T> newFluentList(Class<T> componentClass, T... elements) {
+        return control.newFluentList(componentClass, elements);
+    }
+
+    public <T extends FluentWebElement> FluentList<T> newFluentList(Class<T> componentClass, List<T> elements) {
+        return control.newFluentList(componentClass, elements);
+    }
+
+    public <T extends FluentWebElement> FluentList<T> asFluentList(Class<T> componentClass, WebElement... elements) {
+        return control.asFluentList(componentClass, elements);
+    }
+
+    public <T extends FluentWebElement> FluentList<T> asFluentList(Class<T> componentClass, Iterable<WebElement> elements) {
+        return control.asFluentList(componentClass, elements);
+    }
+
+    public <T extends FluentWebElement> FluentList<T> asFluentList(Class<T> componentClass, List<WebElement> elements) {
+        return control.asFluentList(componentClass, elements);
+    }
+
+    public <T> ComponentList<T> newComponentList(Class<T> componentClass) {
+        return control.newComponentList(componentClass);
+    }
+
+    public <T> ComponentList<T> asComponentList(Class<T> componentClass, WebElement... elements) {
+        return control.asComponentList(componentClass, elements);
+    }
+
+    public <T> ComponentList asComponentList(Class<T> componentClass, Iterable<WebElement> elements) {
+        return control.asComponentList(componentClass, elements);
+    }
+
+    public <T> ComponentList<T> asComponentList(Class<T> componentClass, List<WebElement> elements) {
+        return control.asComponentList(componentClass, elements);
+    }
+
+    public <T> ComponentList<T> newComponentList(Class<T> componentClass, T... componentsList) {
+        return control.newComponentList(componentClass, componentsList);
+    }
+
+    public <T> ComponentList<T> newComponentList(Class<T> componentClass, List<T> componentsList) {
+        return control.newComponentList(componentClass, componentsList);
+    }
+
+    public <L extends List<T>, T> L newComponentList(Class<L> listClass, Class<T> componentClass) {
+        return control.newComponentList(listClass, componentClass);
+    }
+
+    public <L extends List<T>, T> L asComponentList(Class<L> listClass, Class<T> componentClass, WebElement... elements) {
+        return control.asComponentList(listClass, componentClass, elements);
+    }
+
+    public <L extends List<T>, T> L asComponentList(Class<L> listClass, Class<T> componentClass, Iterable<WebElement> elements) {
+        return control.asComponentList(listClass, componentClass, elements);
+    }
+
+    public <L extends List<T>, T> L asComponentList(Class<L> listClass, Class<T> componentClass, List<WebElement> elements) {
+        return control.asComponentList(listClass, componentClass, elements);
+    }
+
+    public <L extends List<T>, T> L newComponentList(Class<L> listClass, Class<T> componentClass, T... componentsList) {
+        return control.newComponentList(listClass, componentClass, componentsList);
+    }
+
+    public <L extends List<T>, T> L newComponentList(Class<L> listClass, Class<T> componentClass, List<T> componentsList) {
+        return control.newComponentList(listClass, componentClass, componentsList);
+    }
+
+    public boolean isComponentClass(Class<?> componentClass) {
+        return control.isComponentClass(componentClass);
+    }
+
+    public Capabilities capabilities() {
+        return control.capabilities();
+    }
+
+    public boolean isComponentListClass(Class<? extends List<?>> componentListClass) {
+        return control.isComponentClass(componentListClass);
+    }
+
+    public String getWebDriver() {
+        return control.getWebDriver();
+    }
+
+    public String getRemoteUrl() {
+        return control.getRemoteUrl();
+    }
+
+    public Capabilities getCapabilities() {
+        return control.getCapabilities();
+    }
+
+    public String getBaseUrl() {
+        return control.getBaseUrl();
+    }
+
+    public ConfigurationProperties.DriverLifecycle getDriverLifecycle() {
+        return control.getDriverLifecycle();
+    }
+
+    public Long getBrowserTimeout() {
+        return control.getBrowserTimeout();
+    }
+
+    public Integer getBrowserTimeoutRetries() {
+        return control.getBrowserTimeoutRetries();
+    }
+
+    public Boolean getDeleteCookies() {
+        return control.getDeleteCookies();
+    }
+
+    public Long getPageLoadTimeout() {
+        return control.getPageLoadTimeout();
+    }
+
+    public Long getImplicitlyWait() {
+        return control.getImplicitlyWait();
+    }
+
+    public Long getScriptTimeout() {
+        return control.getScriptTimeout();
+    }
+
+    public Long getAwaitAtMost() {
+        return control.getAwaitAtMost();
+    }
+
+    public Long getAwaitPollingEvery() {
+        return control.getAwaitPollingEvery();
+    }
+
+    public Boolean getEventsEnabled() {
+        return control.getEventsEnabled();
+    }
+
+    public String getScreenshotPath() {
+        return control.getScreenshotPath();
+    }
+
+    public ConfigurationProperties.TriggerMode getScreenshotMode() {
+        return control.getScreenshotMode();
+    }
+
+    public String getHtmlDumpPath() {
+        return control.getHtmlDumpPath();
+    }
+
+    public ConfigurationProperties.TriggerMode getHtmlDumpMode() {
+        return control.getHtmlDumpMode();
+    }
+
+    public Class<? extends ConfigurationProperties> getConfigurationDefaults() {
+        return control.getConfigurationDefaults();
+    }
+
+    public Class<? extends ConfigurationFactory> getConfigurationFactory() {
+        return control.getConfigurationFactory();
+    }
+
+    public String getCustomProperty(String propertyName) {
+        return control.getCustomProperty(propertyName);
+    }
+
+    public void setWebDriver(String webDriver) {
+        control.setWebDriver(webDriver);
+    }
+
+    public void setBrowserTimeout(Long timeout) {
+        control.setBrowserTimeout(timeout);
+    }
+
+    public void setBrowserTimeoutRetries(Integer retriesNumber) {
+        control.setBrowserTimeoutRetries(retriesNumber);
+    }
+
+    public void setRemoteUrl(String remoteUrl) {
+        control.setRemoteUrl(remoteUrl);
+    }
+
+    public void setCapabilities(Capabilities capabilities) {
+        control.setCapabilities(capabilities);
+    }
+
+    public void setConfigurationFactory(Class<? extends ConfigurationFactory> configurationFactory) {
+        control.setConfigurationFactory(configurationFactory);
+    }
+
+    public void setDriverLifecycle(ConfigurationProperties.DriverLifecycle driverLifecycle) {
+        control.setDriverLifecycle(driverLifecycle);
+    }
+
+    public void setDeleteCookies(Boolean deleteCookies) {
+        control.setDeleteCookies(deleteCookies);
+    }
+
+    public void setBaseUrl(String baseUrl) {
+        control.setBaseUrl(baseUrl);
+    }
+
+    public void setPageLoadTimeout(Long pageLoadTimeout) {
+        control.setPageLoadTimeout(pageLoadTimeout);
+    }
+
+    public void setImplicitlyWait(Long implicitlyWait) {
+        control.setImplicitlyWait(implicitlyWait);
+    }
+
+    public void setAwaitAtMost(Long awaitAtMost) {
+        control.setAwaitAtMost(awaitAtMost);
+    }
+
+    public void setAwaitPollingEvery(Long awaitPollingEvery) {
+        control.setAwaitPollingEvery(awaitPollingEvery);
+    }
+
+    public void setScriptTimeout(Long scriptTimeout) {
+        control.setScriptTimeout(scriptTimeout);
+    }
+
+    public void setEventsEnabled(Boolean eventsEnabled) {
+        control.setEventsEnabled(eventsEnabled);
+    }
+
+    public void setScreenshotPath(String screenshotPath) {
+        control.setScreenshotPath(screenshotPath);
+    }
+
+    public void setScreenshotMode(ConfigurationProperties.TriggerMode screenshotMode) {
+        control.setScreenshotMode(screenshotMode);
+    }
+
+    public void setHtmlDumpPath(String htmlDumpPath) {
+        control.setHtmlDumpPath(htmlDumpPath);
+    }
+
+    public void setHtmlDumpMode(ConfigurationProperties.TriggerMode htmlDumpMode) {
+        control.setHtmlDumpMode(htmlDumpMode);
+    }
+
+    public void setCustomProperty(String key, String value) {
+        control.setCustomProperty(key, value);
     }
 
     @Override
@@ -215,7 +595,7 @@ public class FluentWebElement extends Component
      */
     public <T> T as(Class<T> componentClass) {
         T component = instantiator.newComponent(componentClass, getElement());
-        getFluentControl().injectComponent(component, this, getElement());
+        control.injectComponent(component, this, getElement());
         return component;
     }
 
@@ -466,7 +846,7 @@ public class FluentWebElement extends Component
 
     @Override
     public FluentWebElement frame() {
-        getFluentControl().window().switchTo().frame(this);
+        control.window().switchTo().frame(this);
         return this;
     }
 
