95e950b2e89920ac067ffff4e2d69e25eb05417c
Adrian Koryl <adrian.koryl@gmail.com>
2019-02-27 15:18:58 +0100

    Reworking SeleniumVersionChecker, added more unit tests

--- fluentlenium-core/src/main/java/org/fluentlenium/utils/SeleniumVersionChecker.java
+++ fluentlenium-core/src/main/java/org/fluentlenium/utils/SeleniumVersionChecker.java
@@ -10,9 +10,9 @@ import org.slf4j.LoggerFactory;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -22,51 +22,48 @@ public final class SeleniumVersionChecker {
             "Failed to read Selenium version from your pom.xml."
                     + " Skipped compatibility check."
                     + " Please make sure you are using correct Selenium version - {}";
-    private static final String NOT_SET_MESSAGE =
-            "Cannot find Selenium version property in your pom.xml."
-                    + " Please set it to {}. "
-                    + "You can find example on project main page {}";
     private static final String WRONG_VERSION_MESSAGE =
-            "You are using incompatible Selenium version. Please change it to {}";
+            "You are using incompatible Selenium version. Please change it to {}. "
+                    + "You can find example on project main page {}";
 
     private static final String EXPECTED_VERSION = "3.141.59";
     private static final String SELENIUM_GROUP_ID = "org.seleniumhq.selenium";
     private static final String FL_URL = "https://github.com/FluentLenium/FluentLenium";
     private static final String POM = "pom.xml";
+    private static final String VERSION_REGEX = "^\\$\\{.*}$";
 
     private static final Logger logger = LoggerFactory.getLogger(SeleniumVersionChecker.class);
 
-    private static boolean notifiedAlready;
-    private static boolean isSeleniumVersionFound;
+    static boolean notifiedAlready;
+    static boolean isSeleniumVersionFound;
 
     private SeleniumVersionChecker() {
         // utility class
     }
 
     public static void checkSeleniumVersion() {
+        checkVersionFromMaven(POM);
+    }
+
+    static void checkVersionFromMaven(String pomFile) {
         if (!notifiedAlready) {
             MavenXpp3Reader reader = new MavenXpp3Reader();
             Model model;
 
-            if (new File(POM).exists()) {
-                model = readPom(reader, POM);
+            if (new File(pomFile).exists()) {
+                model = readPom(reader, pomFile);
                 logWarningsWhenSeleniumVersionIsWrong(model);
             }
 
-            if (!isSeleniumVersionFound && new File("../" + POM).exists()) {
-                model = readPom(reader, "../" + POM);
+            if (!isSeleniumVersionFound && new File("../" + pomFile).exists()) {
+                model = readPom(reader, "../" + pomFile);
                 logWarningsWhenSeleniumVersionIsWrong(model);
-            }
-
-            if (!isSeleniumVersionFound) {
-                logger.warn(NOT_SET_MESSAGE, EXPECTED_VERSION, FL_URL);
             }
         }
         notifiedAlready = true;
     }
 
     private static void logWarningsWhenSeleniumVersionIsWrong(Model model) {
-        System.out.println(model);
         if (model != null) {
             String seleniumVersion = retrieveVersionFromPom(model);
 
@@ -74,9 +71,13 @@ public final class SeleniumVersionChecker {
                 return;
             }
             isSeleniumVersionFound = true;
+            if (checkForParametrizedVersion(seleniumVersion)) {
+                String seleniumVersionTemp = seleniumVersion.substring(2, seleniumVersion.length() - 1);
+                seleniumVersion = checkModelForParametrizedValue(seleniumVersionTemp, model);
+            }
 
-            if (!seleniumVersion.equals(EXPECTED_VERSION)) {
-                logger.warn(WRONG_VERSION_MESSAGE, EXPECTED_VERSION);
+            if (!Objects.equals(seleniumVersion, EXPECTED_VERSION)) {
+                logger.warn(WRONG_VERSION_MESSAGE, EXPECTED_VERSION, FL_URL);
             }
         }
     }
@@ -100,7 +101,31 @@ public final class SeleniumVersionChecker {
                 .orElse(null);
     }
 
-    static Model readPom(MavenXpp3Reader reader, String pathToPom) {
+    private static boolean checkForParametrizedVersion(String version) {
+        return version.matches(VERSION_REGEX);
+    }
+
+    private static String checkModelForParametrizedValue(String version, Model model) {
+        String versionProp = null;
+
+        if (model.getProperties() != null) {
+            versionProp = model.getProperties().getProperty(version);
+
+        } else if (System.getProperty(version) != null) {
+            versionProp = System.getProperty(version);
+
+        } else if (model.getProfiles() != null && model.getProfiles().size() > 0) {
+            versionProp = model.getProfiles().stream()
+                    .filter(prof ->
+                            prof.getProperties() != null && prof.getProperties().getProperty(version) != null)
+                    .findAny()
+                    .map(prof -> prof.getProperties().getProperty(version))
+                    .orElse(null);
+        }
+        return versionProp;
+    }
+
+    private static Model readPom(MavenXpp3Reader reader, String pathToPom) {
         Model model;
         try {
             model = reader.read(new FileReader(pathToPom));
--- fluentlenium-core/src/test/java/org/fluentlenium/utils/SeleniumVersionCheckerTest.java
+++ fluentlenium-core/src/test/java/org/fluentlenium/utils/SeleniumVersionCheckerTest.java
@@ -1,29 +1,46 @@
 package org.fluentlenium.utils;
 
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.read.ListAppender;
 import org.apache.maven.model.Model;
 import org.apache.maven.model.io.xpp3.MavenXpp3Reader;
 import org.codehaus.plexus.util.xml.pull.XmlPullParserException;
+import org.junit.Before;
 import org.junit.Test;
+import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 
-import static java.util.Objects.requireNonNull;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.fluentlenium.utils.SeleniumVersionChecker.retrieveVersionFromPom;
 
 public class SeleniumVersionCheckerTest {
 
+    private static final String FAILED_TO_READ_MESSAGE =
+            "Failed to read Selenium version from your pom.xml."
+                    + " Skipped compatibility check."
+                    + " Please make sure you are using correct Selenium version - {}";
+    private static final String WRONG_VERSION_MESSAGE =
+            "You are using incompatible Selenium version. Please change it to {}. "
+                    + "You can find example on project main page {}";
+
     private final static String EXPECTED_VERSION = "3.141.59";
-    private final static String PATH_TO_POM = "org/fluentlenium/utils/";
+    private final static String PATH_TO_POM = "src/test/resources/org/fluentlenium/utils/";
     private final static String POM = "dummy_pom.xml";
 
+    @Before
+    public void reset() {
+        SeleniumVersionChecker.isSeleniumVersionFound = false;
+        SeleniumVersionChecker.notifiedAlready = false;
+    }
+
     @Test
-    public void retrieveVersionFromPomTest() throws IOException, XmlPullParserException {
+    public void retrieveGoodVersionShouldReturnTrueTest() throws IOException, XmlPullParserException {
         MavenXpp3Reader reader = new MavenXpp3Reader();
-        ClassLoader classLoader = getClass().getClassLoader();
-        File file = new File(requireNonNull(classLoader.getResource(PATH_TO_POM + POM)).getFile());
+        File file = new File(PATH_TO_POM + POM);
 
         Model model = reader.read(new FileReader(file));
 
@@ -33,15 +50,101 @@ public class SeleniumVersionCheckerTest {
     }
 
     @Test
-    public void shouldReturnNullWhenVersionNotPresentTest() throws IOException, XmlPullParserException {
+    public void retrieveBadVersionShouldReturnNullTest() throws IOException, XmlPullParserException {
         MavenXpp3Reader reader = new MavenXpp3Reader();
-        ClassLoader classLoader = getClass().getClassLoader();
-        File file = new File(requireNonNull(classLoader.getResource(PATH_TO_POM + "dummy/" + POM)).getFile());
+        File file = new File(PATH_TO_POM + "wrong/" + POM);
+
+        Model model = reader.read(new FileReader(file));
+
+        String actualVersion = retrieveVersionFromPom(model);
+
+        assertThat(actualVersion).isNotEqualTo(EXPECTED_VERSION);
+    }
+
+    @Test
+    public void retrieveVersionShouldReturnNullWhenVersionNotPresentTest() throws IOException, XmlPullParserException {
+        MavenXpp3Reader reader = new MavenXpp3Reader();
+        File file = new File(PATH_TO_POM + "dummy/" + POM);
 
         Model model = reader.read(new FileReader(file));
 
         String actualVersion = retrieveVersionFromPom(model);
 
         assertThat(actualVersion).isNull();
+    }
+
+    @Test
+    public void shouldLogWrongVersionOfLibraryTest() {
+
+        Logger fooLogger = (Logger) LoggerFactory.getLogger(SeleniumVersionChecker.class);
+
+        ListAppender<ILoggingEvent> listAppender = new ListAppender<>();
+        listAppender.start();
+
+        fooLogger.addAppender(listAppender);
+
+        SeleniumVersionChecker.checkVersionFromMaven(PATH_TO_POM + "wrong/" + POM);
+
+        assertThat(listAppender.list)
+                .extracting(ILoggingEvent::getMessage)
+                .hasSize(1)
+                .element(0)
+                .isEqualTo(WRONG_VERSION_MESSAGE);
+    }
+
+    @Test
+    public void shouldLogMessageOnlyOnceTest() {
+
+        Logger fooLogger = (Logger) LoggerFactory.getLogger(SeleniumVersionChecker.class);
+
+        ListAppender<ILoggingEvent> listAppender = new ListAppender<>();
+        listAppender.start();
+
+        fooLogger.addAppender(listAppender);
+
+        SeleniumVersionChecker.checkVersionFromMaven(PATH_TO_POM + "wrong/" + POM);
+        SeleniumVersionChecker.checkVersionFromMaven(PATH_TO_POM + "wrong/" + POM);
+
+        assertThat(listAppender.list)
+                .extracting(ILoggingEvent::getMessage)
+                .hasSize(1)
+                .element(0)
+                .isEqualTo(WRONG_VERSION_MESSAGE);
+    }
+
+    @Test
+    public void shouldNotLogAnythingWhenFileIsNotPresentTest() {
+
+        Logger fooLogger = (Logger) LoggerFactory.getLogger(SeleniumVersionChecker.class);
+
+        ListAppender<ILoggingEvent> listAppender = new ListAppender<>();
+        listAppender.start();
+
+        fooLogger.addAppender(listAppender);
+
+        SeleniumVersionChecker.checkVersionFromMaven(PATH_TO_POM + "wrong/not_exisitng.xml");
+
+        assertThat(listAppender.list)
+                .extracting(ILoggingEvent::getMessage)
+                .hasSize(0);
+    }
+
+    @Test
+    public void shouldLogMessageWhenWrongFileWasPassedTest() {
+
+        Logger fooLogger = (Logger) LoggerFactory.getLogger(SeleniumVersionChecker.class);
+
+        ListAppender<ILoggingEvent> listAppender = new ListAppender<>();
+        listAppender.start();
+
+        fooLogger.addAppender(listAppender);
+
+        SeleniumVersionChecker.checkVersionFromMaven(PATH_TO_POM + "dummy_resource.json");
+
+        assertThat(listAppender.list)
+                .extracting(ILoggingEvent::getMessage)
+                .hasSize(1)
+                .element(0)
+                .isEqualTo(FAILED_TO_READ_MESSAGE);
     }
 }
