edd4b36fc699ab8574d964691c80c517606d6dff
Tamas Balog <picimako@gmail.com>
2019-03-08 21:12:13 +0100

    Remove unused imports

--- fluentlenium-core/src/main/java/org/fluentlenium/core/inject/NoInject.java
+++ fluentlenium-core/src/main/java/org/fluentlenium/core/inject/NoInject.java
@@ -3,9 +3,7 @@ package org.fluentlenium.core.inject;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
-import static java.lang.annotation.ElementType.CONSTRUCTOR;
 import static java.lang.annotation.ElementType.FIELD;
-import static java.lang.annotation.ElementType.METHOD;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 /**
