6a5822f26294e168561cf729fd51c00b43101dae
Tamas Balog <picimako@gmail.com>
2019-03-08 20:35:03 +0100

    Extend NoInject and Parent annotation javadoc

--- fluentlenium-core/src/main/java/org/fluentlenium/core/inject/NoInject.java
+++ fluentlenium-core/src/main/java/org/fluentlenium/core/inject/NoInject.java
@@ -9,7 +9,13 @@ import static java.lang.annotation.ElementType.METHOD;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 /**
- * Disable FluentLenium injection for this field.
+ * When this annotation is applied on a field FluentLenium injection is disabled for that.
+ * <p>
+ * It may be useful in scenarios when one wants to have a simple instance field setting it manually, and not relying on injection
+ * either using a Selenium {@code Find...} annotation, or without using one relying on the field name based element lookup.
+ * <p>
+ * With that said if a field is marked as {@code NoInject}, none of the Selenium {@code Find...} annotations are necessary
+ * on that field, because it won't have any effect.
  */
 @Target(FIELD)
 @Retention(RUNTIME)
--- fluentlenium-core/src/main/java/org/fluentlenium/core/inject/Parent.java
+++ fluentlenium-core/src/main/java/org/fluentlenium/core/inject/Parent.java
@@ -9,7 +9,50 @@ import static java.lang.annotation.ElementType.METHOD;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 /**
- * Mark a field with this annotation to inject the parent container.
+ * Mark a field whose class declaration is annotated with one of Selenium's {@code Find...} annotation,
+ * with this annotation to inject the parent container. Parent container in this case means parent in the HTML DOM.
+ * <p>
+ * It doesn't matter if the parent is a direct parent or is any element that is upper in the DOM relative to the current element.
+ * What element the parent represents depends on the {@code Find...} annotation applied on the parent component or page class.
+ * <p>
+ * <h3>Examples</h3>
+ * For an HTML snippet like
+ * <pre>
+ * &lt;div id="homepage">
+ *      &lt;div class="component">
+ *          &lt;div class="sub-component">
+ *              ...
+ *          &lt;/div>
+ *      &lt;/div>
+ * &lt;/div>
+ * </pre>
+ * there can be a custom component for the {@code div.component} element:
+ * <pre>
+ * &#064;FindBy(className = "component")
+ * public class Component extends FluentWebElement {
+ *      &#064;Parent
+ *      public Homepage homepage;
+ *      ...
+ * }
+ * </pre>
+ * for which a sub-component can include it as a parent component:
+ * <pre>
+ * &#064;FindBy(className = "sub-component")
+ * public class SubComponent extends FluentWebElement {
+ *      &#064;Parent
+ *      public Component parent;
+ *      ...
+ * }
+ * </pre>
+ * Similarly a parent can be created for {@code Component} in the form of a page:
+ * <pre>
+ * &#064;FindBy(id = "homepage")
+ * public class Homepage extends FluentPage {
+ *      ...
+ * }
+ * </pre>
+ * This structure can be achieved with any page-component/component-page relationship using custom
+ * {@link org.fluentlenium.core.FluentPage} and {@link org.fluentlenium.core.domain.FluentWebElement} implementations.
  */
 @Target(FIELD)
 @Retention(RUNTIME)
