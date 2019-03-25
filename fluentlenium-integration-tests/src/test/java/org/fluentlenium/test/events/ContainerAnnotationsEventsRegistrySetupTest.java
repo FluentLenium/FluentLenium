0ba15a32c965a91a47742bee3c64d0c9e8894769
slawekradzyminski <slawomir.radzyminski@gmail.com>
2019-03-25 19:15:19 +0100

    Minor - added missing assertions to tests I changed

--- fluentlenium-integration-tests/src/test/java/org/fluentlenium/test/events/ContainerAnnotationsEventsRegistrySetupTest.java
+++ fluentlenium-integration-tests/src/test/java/org/fluentlenium/test/events/ContainerAnnotationsEventsRegistrySetupTest.java
@@ -2,13 +2,16 @@ package org.fluentlenium.test.events;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import net.jcip.annotations.NotThreadSafe;
 import org.fluentlenium.core.events.EventsRegistry;
 import org.fluentlenium.core.events.annotations.AfterClickOn;
 import org.fluentlenium.core.events.annotations.BeforeClickOn;
 import org.fluentlenium.test.IntegrationFluentTest;
 import com.google.common.collect.ImmutableList;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,31 +20,55 @@ import org.slf4j.LoggerFactory;
  * Integration test for {@link org.fluentlenium.core.events.ContainerAnnotationsEventsRegistry} to validate that
  * listeners based on method annotations are registered in {@link EventsRegistry} with their priority.
  */
+@NotThreadSafe
 class ContainerAnnotationsEventsRegistrySetupTest extends IntegrationFluentTest {
 
-    private static final Logger logger = LoggerFactory.getLogger(ContainerAnnotationsEventsRegistrySetupTest.class);
+    private static final Logger logger =
+            LoggerFactory.getLogger(ContainerAnnotationsEventsRegistrySetupTest.class);
 
+    private static final String BEFORE = "Before";
+    private static final String FIRST_AFTER = "First after";
+    private static final String SECOND_AFTER = "Second after";
+
+    private List<String> executed;
+
+    @BeforeEach
+    void setUp() {
+        executed = new ArrayList<>();
+    }
 
     @BeforeClickOn
     void beforeClickOnListenerMethod() {
         logger.info("About to click on something...");
+        executed.add(BEFORE);
     }
 
     @AfterClickOn(10)
     void afterClickOnListenerMethodWithLowerPriority() {
         logger.info("Clicked successfully. FluentLenium is awesome! :)");
+        executed.add(FIRST_AFTER);
     }
 
     @AfterClickOn(5)
     void afterClickOnListenerMethod() {
         logger.info("Isn't it?");
+        executed.add(SECOND_AFTER);
     }
 
     @Test
-    void shouldRegisterAnnotationBasedListeners() {
+    void shouldRegisterAnnotationBasedListenersAndExecuteThem() {
         assertEventsInitialState();
         goTo(DEFAULT_URL);
         el("button").click();
+        assertThat(executed).hasSize(3)
+                .containsExactly(BEFORE, FIRST_AFTER, SECOND_AFTER);
+    }
+
+    @Test
+    void shouldRegisterAnnotationBasedListenersAndDoNotExecuteThem() {
+        assertEventsInitialState();
+        goTo(DEFAULT_URL);
+        assertThat(executed).isEmpty();
     }
 
     private void assertEventsInitialState() {
