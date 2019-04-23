82ac760fdafbe6fc033dc9c2c800410997d96c0a
Tamas Balog <picimako@gmail.com>
2019-04-23 20:15:53 +0200

    Use temp file absolute path

--- fluentlenium-core/src/test/java/org/fluentlenium/TempFileCreatorTest.java
+++ fluentlenium-core/src/test/java/org/fluentlenium/TempFileCreatorTest.java
@@ -2,11 +2,13 @@ package org.fluentlenium;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.apache.commons.io.FileUtils;
 import org.junit.After;
 import org.junit.Test;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 /**
  * Unit test for temp file creation.
@@ -24,6 +26,19 @@ public class TempFileCreatorTest {
     public void shouldTakeHtmlDumpWithNoConfiguration() throws IOException {
         destinationFile = File.createTempFile("htmldump.html", "");
 
+        destinationFile.delete();
+
+        writeToFile(destinationFile.getAbsolutePath());
+
         assertThat(destinationFile).exists();
+    }
+
+    private void writeToFile(String fileName) {
+        File destFile = new File(fileName);
+        try {
+            FileUtils.write(destFile, "Some html dump.", StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
