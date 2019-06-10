4aed8d460e959a6aae1518fe374541c8524a0d30
Slawek <slawomir.radzyminski@gmail.com>
2019-06-10 11:25:13 +0200

    #859 - Enforce Windows 10 & IE11 for E2E tests

--- examples/spring/src/main/java/org/fluentlenium/example/spring/config/browser/IE.java
+++ examples/spring/src/main/java/org/fluentlenium/example/spring/config/browser/IE.java
@@ -9,6 +9,8 @@ public class IE implements IBrowser {
     public Capabilities getBrowserCapabilities() {
         InternetExplorerOptions internetExplorerOptions = new InternetExplorerOptions();
         internetExplorerOptions.destructivelyEnsureCleanSession();
+        internetExplorerOptions.setCapability("browser_version", "11.0");
+        internetExplorerOptions.setCapability("os_version", "10");
         return internetExplorerOptions;
     }
 
