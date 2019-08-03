763155f28211a4e0de97e962c5ece49d0002df71
picimako <picimako@gmail.com>
2019-08-03 08:18:52 +0200

    #904: Remove unused imports and added junit Parameterized.Parameter to ignored its visibility modifiers

--- fluentlenium-core/src/test/java/org/fluentlenium/configuration/WebDriversRegistryImplTest.java
+++ fluentlenium-core/src/test/java/org/fluentlenium/configuration/WebDriversRegistryImplTest.java
@@ -10,14 +10,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
-import org.openqa.selenium.ie.InternetExplorerDriver;
-import org.openqa.selenium.opera.OperaDriver;
-import org.openqa.selenium.phantomjs.PhantomJSDriver;
-import org.openqa.selenium.safari.SafariDriver;
 
 /**
  * Unit test for {@link WebDriversRegistryImpl}.
