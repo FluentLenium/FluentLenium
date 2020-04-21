4abbbc330dddea92c503d47d739d686c1c25e62f
Tamás Balog <picimako@gmail.com>
2020-04-21 11:03:04 +0200

    #1055: Misc. javadoc extensions

--- fluentlenium-core/src/main/java/org/fluentlenium/core/ScreenshotNotCreatedException.java
+++ fluentlenium-core/src/main/java/org/fluentlenium/core/ScreenshotNotCreatedException.java
@@ -2,6 +2,9 @@ package org.fluentlenium.core;
 
 import java.io.IOException;
 
+/**
+ * Exception type to be thrown when a screenshot taking process couldn't finish.
+ */
 public class ScreenshotNotCreatedException extends RuntimeException {
 
     public ScreenshotNotCreatedException(String errorMessage, IOException e) {
