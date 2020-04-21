5fe51f67f5aa2cff81feae7087832d1793be624c
Tamás Balog <picimako@gmail.com>
2020-04-21 10:54:59 +0200

    #1055: Extend javadoc of Label, LabelHint and LabelAnnotations

--- fluentlenium-core/src/main/java/org/fluentlenium/core/annotation/Label.java
+++ fluentlenium-core/src/main/java/org/fluentlenium/core/annotation/Label.java
@@ -7,8 +7,58 @@ import java.lang.annotation.Target;
 
 /**
  * Define a label on an injected {@link org.fluentlenium.core.domain.FluentWebElement} or
- * {@link org.fluentlenium.core.domain.FluentList}
+ * {@link org.fluentlenium.core.domain.FluentList}.
+ * <p>
+ * By default the injected element's original {@code toString()} value is returned but with
+ * having the {@code @Label} annotation with different configurations that toString value can
+ * be overridden. Using it can provide more meaningful error messages during test executuon and debugging.
+ * <p>
+ * {@code FluentWebElement} Examples:
+ * <pre>
+ * public class HomepageHeader {
  *
+ *      &#064;FindBy(css = "#header")
+ *      private FluentWebElement header;
+ *      //toString(): By.cssSelector: #header (first) (Lazy Element)
+ *
+ *      &#064;FindBy(css = "#header")
+ *      &#064;Label
+ *      private FluentWebElement headerDefaultLabel;
+ *      //toString(): HomepageHeader.headerDefaultLabel (Lazy Element)
+ *
+ *      &#064;FindBy(css = "#header")
+ *      &#064;Label("customLabel")
+ *      private FluentWebElement headerCustomLabel;
+ *      //toString(): customLabel (Lazy Element)
+ * }
+ * </pre>
+ * <p>
+ * {@code FluentList} Examples:
+ * <pre>
+ * public class HomepageHeader {
+ *      &#064;FindBy(css = ".footer-link")
+ *      private FluentList&lt;FluentWebElement> footerLinks;
+ *      //toString(): By.cssSelector: .footer-link (&lt;toString() of the underlying list of FluentWebElements>)
+ *
+ *      &#064;FindBy(css = ".footer-link")
+ *      &#064;Label
+ *      private FluentList&lt;FluentWebElement> footerLinksDefaultLabel;
+ *      //toString(): HomepageHeader.footerLinksDefaultLabel (&lt;toString() of the underlying list of FluentWebElements>)
+ *
+ *      &#064;FindBy(css = ".footer-link")
+ *      &#064;Label("customLabel")
+ *      private FluentList&lt;FluentWebElement> footerLinksCustomLabel;
+ *      //toString(): customLabel (&lt;toString() of the underlying list of FluentWebElements>)
+ * }
+ * </pre>
+ * <p>
+ * This annotation is independent from the {@link LabelHint} annotation. Each one can be used without the other.
+ * <p>
+ * Defining a label can also be done inline on an a {@code FluentWebElement} or {@code FluentList} by calling the {@code withLabel()}
+ * method on it.
+ *
+ * @see org.fluentlenium.core.inject.LabelAnnotations
+ * @see org.fluentlenium.core.label.FluentLabelImpl
  * @see org.fluentlenium.core.label.FluentLabel#withLabel(String)
  */
 @Retention(RetentionPolicy.RUNTIME)
