04d776e06f462c2667f97b6851c3907f9f1306d7
Alphonse Bendt <alphonse.bendt@gmail.com>
2021-05-26 01:31:14 +0200

    enable driver per thread https://github.com/FluentLenium/FluentLenium/pull/1251#discussion_r639217748

--- fluentlenium-kotest/src/main/kotlin/org/fluentlenium/adapter/kotest/internal/KoTestFluentAdapter.kt
+++ fluentlenium-kotest/src/main/kotlin/org/fluentlenium/adapter/kotest/internal/KoTestFluentAdapter.kt
@@ -1,5 +1,6 @@
 package org.fluentlenium.adapter.kotest.internal
 
+import io.kotest.common.ExperimentalKotest
 import io.kotest.core.listeners.TestListener
 import io.kotest.core.spec.Spec
 import io.kotest.core.test.TestCase
@@ -15,7 +16,6 @@ import org.fluentlenium.adapter.IFluentAdapter
 import org.fluentlenium.adapter.TestRunnerCommon.*
 import org.fluentlenium.adapter.sharedwebdriver.SharedWebDriverContainer
 import org.fluentlenium.configuration.Configuration
-import org.fluentlenium.configuration.ConfigurationProperties
 import org.fluentlenium.utils.ScreenshotUtil
 import org.fluentlenium.utils.SeleniumVersionChecker
 import java.util.concurrent.atomic.AtomicReference
@@ -27,8 +27,10 @@ internal class KoTestFluentAdapter constructor(var useConfigurationOverride: () 
     val sharedMutator = DefaultSharedMutator()
 
     /**
-     * KoTest doc states that tests inside a Spec are always executed sequentially (https://kotest.io/docs/framework/project-config.html#parallelism)
-     * So it should be safe to store the current test name like this.
+     * Kotest will use the same thread for all tests in a spec as long as Kotests configuration option
+     * dispatcherAffinity == true. With this assumption its safe to use an AtomicReference here to
+     * store the name of the currently active test. we will check that configuration and fail the execution if set
+     * to false.
      */
     val currentTestName = AtomicReference<String>()
 
@@ -36,17 +38,9 @@ internal class KoTestFluentAdapter constructor(var useConfigurationOverride: () 
 
     override fun getConfiguration(): Configuration = configurationOverride
 
+    @OptIn(ExperimentalKotest::class)
     fun listener(): TestListener = object : TestListener {
         override suspend fun beforeSpec(spec: Spec) {
-            require(driverLifecycle != ConfigurationProperties.DriverLifecycle.THREAD) {
-                // not sure about this:
-                // as the tests are executed within a Coroutine it
-                // could be possible that the "same" thread executes mulitple tests and/or that one test is executed
-                // by mulitple Coroutines with different underlying Threads. need to investigate
-
-                "DriverLifecyle $driverLifecycle not supported"
-            }
-
             withContext(Dispatchers.IO) {
                 SeleniumVersionChecker.checkSeleniumVersion()
             }
@@ -56,13 +50,21 @@ internal class KoTestFluentAdapter constructor(var useConfigurationOverride: () 
             if (testCase.type == TestType.Container)
                 return
 
+            val singleThreadPerTest =
+                testCase.spec.dispatcherAffinity ?: testCase.spec.dispatcherAffinity()
+                ?: io.kotest.core.config.configuration.dispatcherAffinity
+
+            require(singleThreadPerTest) {
+                "fluentlenium-kotest is incompatible with dispatcherAffinity=false. set to true!"
+            }
+
             val thisListener = this
             val testClass = testCase.spec.javaClass
             val testName = testCase.displayName
 
             currentTestName.set(testName)
 
-            val driver = withContext(Dispatchers.IO) {
+            val driver =
                 getTestDriver(
                     testCase.spec.javaClass,
                     testName,
@@ -71,7 +73,7 @@ internal class KoTestFluentAdapter constructor(var useConfigurationOverride: () 
                     configuration,
                     sharedMutator.getEffectiveParameters(testClass, testName, driverLifecycle)
                 )
-            }
+
             val fluent = initFluent(driver.driver)
             fluent.inject(testCase.spec)
         }
@@ -84,18 +86,16 @@ internal class KoTestFluentAdapter constructor(var useConfigurationOverride: () 
             val testClass = testCase.spec.javaClass
             val testName = testCase.displayName
 
-            withContext(Dispatchers.IO) {
-                if (result.status == TestStatus.Error || result.status == TestStatus.Failure) {
-                    failed(result.error, testClass, testName)
-                }
-
-                val sharedWebDriver = SharedWebDriverContainer.INSTANCE
-                    .getDriver(sharedMutator.getEffectiveParameters(testClass, testName, driverLifecycle))
-
-                quitMethodAndThreadDrivers(driverLifecycle, sharedWebDriver)
-                deleteCookies(sharedWebDriver, configuration)
-                releaseFluent()
+            if (result.status == TestStatus.Error || result.status == TestStatus.Failure) {
+                failed(result.error, testClass, testName)
             }
+
+            val sharedWebDriver = SharedWebDriverContainer.INSTANCE
+                .getDriver(sharedMutator.getEffectiveParameters(testClass, testName, driverLifecycle))
+
+            quitMethodAndThreadDrivers(driverLifecycle, sharedWebDriver)
+            deleteCookies(sharedWebDriver, configuration)
+            releaseFluent()
 
             currentTestName.set(null)
         }
