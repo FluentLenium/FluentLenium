98a9f7afdf8764e729e7f9525f98d0819ba3f7b3
Alphonse Bendt <alphonse.bendt@gmail.com>
2021-06-17 21:39:28 +0200

    allow to provide a custom WebDriver instance per test by overriding the newWebDriver method (#1279)

---
+++ fluentlenium-kotest/src/test/kotlin/org/fluentlenium/adapter/kotest/describespec/CanProvideCustomWebDriverSpec.kt
@@ -0,0 +1,24 @@
+package org.fluentlenium.adapter.kotest.describespec
+
+import io.kotest.matchers.shouldBe
+import org.fluentlenium.adapter.kotest.FluentDescribeSpec
+import org.fluentlenium.adapter.kotest.TestConstants.DEFAULT_URL
+import org.openqa.selenium.WebDriver
+
+class CanProvideCustomWebDriverSpec : FluentDescribeSpec() {
+
+    var createCount = 0
+
+    override fun newWebDriver(): WebDriver {
+        createCount++
+        return super.newWebDriver()
+    }
+
+    init {
+        it("we can provide a custom WebDriver by overriding a method") {
+            goTo(DEFAULT_URL)
+
+            createCount shouldBe 1
+        }
+    }
+}
