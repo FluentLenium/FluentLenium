4c403f7aad2d30dfbf5d8a9afdec010254c9ec4f
Alphonse Bendt <alphonse.bendt@gmail.com>
2021-07-04 14:48:38 +0200

    more testcases

--- fluentlenium-kotest-assertions/src/test/kotlin/org/fluentlenium/kotest/matchers/el/FluentWebElementWaitHookSpec.kt
+++ fluentlenium-kotest-assertions/src/test/kotlin/org/fluentlenium/kotest/matchers/el/FluentWebElementWaitHookSpec.kt
@@ -9,4 +9,16 @@ class FluentWebElementWaitHookSpec : MatcherBase({
     "can check for absent elements when using waithook" {
         el("#doesNotExist") shouldNot bePresent()
     }
+
+    "can check for not displayed elements when using waithook" {
+        el("#non_display") shouldNot beDisplayed()
+    }
+
+    "can check for not enabled elements when using waithook" {
+        el("#disabled") shouldNot beEnabled()
+    }
+
+    "can check for not selected elements when using waithook" {
+        el("#disabled") shouldNot beSelected()
+    }
 })
