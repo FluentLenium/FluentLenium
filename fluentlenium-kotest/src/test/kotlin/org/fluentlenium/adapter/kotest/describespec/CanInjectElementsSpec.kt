b9fd771224a5d5423a11a81dbdafa898515aa7f4
Alphonse Bendt <alphonse.bendt@gmail.com>
2021-07-25 17:46:03 +0200

    cleanup test

--- fluentlenium-kotest/src/test/kotlin/org/fluentlenium/adapter/kotest/describespec/CanInjectElementsSpec.kt
+++ fluentlenium-kotest/src/test/kotlin/org/fluentlenium/adapter/kotest/describespec/CanInjectElementsSpec.kt
@@ -6,7 +6,6 @@ import org.fluentlenium.adapter.kotest.FluentDescribeSpec
 import org.fluentlenium.adapter.kotest.TestConstants.DEFAULT_URL
 import org.fluentlenium.core.domain.FluentList
 import org.fluentlenium.core.domain.FluentWebElement
-import org.fluentlenium.core.script.JavascriptControl
 import org.openqa.selenium.support.FindBy
 
 class CanInjectElementsSpec : FluentDescribeSpec() {
@@ -20,14 +19,9 @@ class CanInjectElementsSpec : FluentDescribeSpec() {
     init {
         it("Title should be correct") {
             goTo(DEFAULT_URL)
-            highlightElement(names.first())
             names.fill().with("FluentLenium")
             name.value() shouldBe "FluentLenium"
             window().title() shouldContain "Fluent"
         }
-    }
-
-    fun JavascriptControl.highlightElement(element: FluentWebElement) {
-        executeScript("arguments[0].style.border = '3px solid red';", element.element)
     }
 }
