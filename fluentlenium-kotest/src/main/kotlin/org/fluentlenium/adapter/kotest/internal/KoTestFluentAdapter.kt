9ed07edc52a73d26b23072758cb59e53c6abcb75
Alphonse Bendt <alphonse.bendt@gmail.com>
2021-12-01 18:25:40 +0100

    fix tests

--- fluentlenium-kotest/src/main/kotlin/org/fluentlenium/adapter/kotest/internal/KoTestFluentAdapter.kt
+++ fluentlenium-kotest/src/main/kotlin/org/fluentlenium/adapter/kotest/internal/KoTestFluentAdapter.kt
@@ -54,8 +54,8 @@ internal class KoTestFluentAdapter constructor(var useConfigurationOverride: () 
             this@KoTestFluentAdapter.afterSpec(spec)
         }
 
-        override suspend fun beforeTest(testCase: TestCase) {
-            this@KoTestFluentAdapter.beforeTest(testCase)
+        override suspend fun beforeAny(testCase: TestCase) {
+            this@KoTestFluentAdapter.beforeAny(testCase)
         }
 
         override suspend fun afterEach(testCase: TestCase, result: TestResult) {
@@ -70,7 +70,7 @@ internal class KoTestFluentAdapter constructor(var useConfigurationOverride: () 
     }
 
     @ExperimentalKotest
-    fun beforeTest(testCase: TestCase) {
+    fun beforeAny(testCase: TestCase) {
         if (testCase.type == TestType.Container)
             return
 
