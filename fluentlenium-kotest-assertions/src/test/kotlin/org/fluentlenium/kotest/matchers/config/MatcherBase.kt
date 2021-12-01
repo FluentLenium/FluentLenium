663ec7a7e75aa482a619f811b482f203482015a9
Alphonse Bendt <alphonse.bendt@gmail.com>
2021-12-01 19:29:33 +0100

    fix test

--- fluentlenium-kotest-assertions/src/test/kotlin/org/fluentlenium/kotest/matchers/config/MatcherBase.kt
+++ fluentlenium-kotest-assertions/src/test/kotlin/org/fluentlenium/kotest/matchers/config/MatcherBase.kt
@@ -1,6 +1,7 @@
 package org.fluentlenium.kotest.matchers.config
 
 import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.listeners.BeforeTestListener
 import io.kotest.core.test.TestCase
 import org.fluentlenium.adapter.kotest.FluentStringSpec
 import org.fluentlenium.utils.UrlUtils
@@ -12,8 +13,12 @@ open class MatcherBase(
     fun goToFile(file: String) =
         goTo(UrlUtils.getAbsoluteUrlFromFile(file))
 
-    override fun beforeTest(testCase: TestCase) {
-        goToFile("index.html")
+    init {
+        register(object : BeforeTestListener {
+            override suspend fun beforeTest(testCase: TestCase) {
+                goToFile("index.html")
+            }
+        })
     }
 }
 
