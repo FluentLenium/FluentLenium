3b9443281668a44aa90e60bebb811675e10acd7c
Alphonse Bendt <alphonse.bendt@gmail.com>
2021-12-27 13:20:08 +0100

    fix test URL

--- fluentlenium-kotest-assertions/src/test/kotlin/org/fluentlenium/kotest/matchers/config/pages/FluentleniumPage.kt
+++ fluentlenium-kotest-assertions/src/test/kotlin/org/fluentlenium/kotest/matchers/config/pages/FluentleniumPage.kt
@@ -3,5 +3,5 @@ package org.fluentlenium.kotest.matchers.config.pages
 import org.fluentlenium.core.FluentPage
 import org.fluentlenium.core.annotation.PageUrl
 
-@PageUrl("https://fluentlenium.org")
+@PageUrl("https://fluentlenium.com/")
 class FluentleniumPage : FluentPage()
