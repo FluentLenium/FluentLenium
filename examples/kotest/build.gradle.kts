9b915c5a9c24bedbf8ce6bcfe4b464ac65f561cf
Alphonse Bendt <alphonse.bendt@gmail.com>
2022-03-01 10:19:55 +0100

    Update selenium dev tools (#1493)

--- examples/kotest/build.gradle.kts
+++ examples/kotest/build.gradle.kts
@@ -51,7 +51,7 @@ dependencies {
     val seleniumVersion = "4.1.2"
     testImplementation("org.seleniumhq.selenium:selenium-api:$seleniumVersion")
     testImplementation("org.seleniumhq.selenium:selenium-chrome-driver:$seleniumVersion")
-    testRuntimeOnly("org.seleniumhq.selenium:selenium-devtools-v95:$seleniumVersion")
+    testRuntimeOnly("org.seleniumhq.selenium:selenium-devtools-v97:$seleniumVersion")
 
     testImplementation("io.kotest.extensions:kotest-extensions-testcontainers:1.2.1")
     testImplementation("org.testcontainers:selenium:1.16.3")
