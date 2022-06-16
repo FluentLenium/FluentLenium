f77cdb599703d324d839eaab8e17266c36201703
Alphonse Bendt <370821+abendt@users.noreply.github.com>
2022-06-16 10:19:10 +0200

    determine fluentlenium version dynamically (#1602)

--- examples/kotest/build.gradle.kts
+++ examples/kotest/build.gradle.kts
@@ -36,7 +36,7 @@ tasks.withType<Test>().configureEach {
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
 
-    val fluentleniumVersion = "5.0.5-SNAPSHOT"
+    val fluentleniumVersion = properties["fluentlenium.version"] ?: "5.0.4"
     testImplementation("org.fluentlenium:fluentlenium-kotest:$fluentleniumVersion")
     testImplementation("org.fluentlenium:fluentlenium-kotest-assertions:$fluentleniumVersion")
 
