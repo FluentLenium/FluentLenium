2b76cdd5e19470529b4685fd4a0b0d0b273eb0c8
Alphonse Bendt <alphonse.bendt@gmail.com>
2022-07-16 11:02:34 +0200

    add javadoc

--- fluentlenium-core/src/main/java/org/fluentlenium/core/FluentDriverScreenshotPersister.java
+++ fluentlenium-core/src/main/java/org/fluentlenium/core/FluentDriverScreenshotPersister.java
@@ -1,10 +1,8 @@
 package org.fluentlenium.core;
 
-import static java.util.Objects.requireNonNull;
-
+import org.apache.commons.io.FileUtils;
 import org.fluentlenium.configuration.Configuration;
 import org.fluentlenium.utils.ImageUtils;
-import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.UnhandledAlertException;
@@ -15,6 +13,8 @@ import org.slf4j.LoggerFactory;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Paths;
+
+import static java.util.Objects.requireNonNull;
 
 /**
  * Persists a screenshot to a target file.
@@ -38,6 +38,7 @@ public class FluentDriverScreenshotPersister {
      * otherwise the argument file name will be concatenated to the screenshot path to create the destination file.
      *
      * @param fileName the target file to save the screenshot to
+     * @return the screenshot file
      * @throws ScreenshotNotCreatedException when an error occurs during taking the screenshot
      */
     public File persistScreenshot(String fileName) {
