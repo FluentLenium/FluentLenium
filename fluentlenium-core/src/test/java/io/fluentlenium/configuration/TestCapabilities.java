5919482927c06ece81397a8a5568d3454794328f
Alphonse Bendt <alphonse.bendt@gmail.com>
2023-04-03 23:05:24 +0200

    cherry pick

--- fluentlenium-core/src/test/java/io/fluentlenium/configuration/TestCapabilities.java
+++ fluentlenium-core/src/test/java/io/fluentlenium/configuration/TestCapabilities.java
@@ -1,23 +1,12 @@
 package io.fluentlenium.configuration;
 
 import org.openqa.selenium.Capabilities;
-import org.openqa.selenium.Platform;
 
 import java.util.Map;
 
 public class TestCapabilities implements Capabilities {
     @Override
     public String getBrowserName() {
-        return null;
-    }
-
-    // @Override
-    public Platform getPlatform() {
-        return null;
-    }
-
-    // @Override
-    public String getVersion() {
         return null;
     }
 
