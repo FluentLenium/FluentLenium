7febe56798dd58cf55e79212d936245e1049fc84
Alphonse Bendt <alphonse.bendt@gmail.com>
2023-04-04 10:35:22 +0200

    screenshots

--- examples/quickstart-firefox/src/test/java/io/fluentlenium/examples/test/basic/DuckDuckGoHeadlessFirefoxTest.kt
+++ examples/quickstart-firefox/src/test/java/io/fluentlenium/examples/test/basic/DuckDuckGoHeadlessFirefoxTest.kt
@@ -1,9 +1,12 @@
 package io.fluentlenium.examples.test.basic
 
+import io.fluentlenium.configuration.ConfigurationProperties
 import io.fluentlenium.core.annotation.Page
 import io.fluentlenium.examples.pages.basic.DuckDuckMainPage
 import io.fluentlenium.examples.test.AbstractFirefoxTest
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.openqa.selenium.Dimension
 import org.openqa.selenium.WebDriver
 import org.openqa.selenium.firefox.FirefoxDriver
 import org.openqa.selenium.firefox.FirefoxOptions
@@ -16,10 +19,19 @@ class DuckDuckGoHeadlessFirefoxTest : AbstractFirefoxTest() {
     override fun newWebDriver(): WebDriver =
         FirefoxDriver(FirefoxOptions().apply {
             addArguments("-headless")
-        })
+        }).also {
+            it.manage().window().size = Dimension(1400, 1200)
+        }
+
+    @BeforeEach
+    fun setUp() {
+        screenshotMode = ConfigurationProperties.TriggerMode.AUTOMATIC_ON_FAIL
+        screenshotPath = "build/screenshots"
+    }
 
     @Test
     fun titleOfDuckDuckGoShouldContainSearchQueryName() {
+
         val searchPhrase = "searchPhrase"
         goTo(onDuckDuckGoMainPage)
 
