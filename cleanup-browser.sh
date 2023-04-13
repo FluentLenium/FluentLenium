f0fbe095d0a72d07e79e1c2dfd9bb1a5a94c73d5
Alphonse Bendt <alphonse.bendt@gmail.com>
2023-04-13 23:00:17 +0200

    debug

--- cleanup-browser.sh
+++ cleanup-browser.sh
@@ -2,10 +2,11 @@
 
 echo "killing orphan chrome* processes with fire"
 
+set +e
 killall -qv -s SIGKILL chromedriver
 killall -qv -s SIGKILL google-chrome
 killall -qv -s SIGKILL chrome
 killall -qv -s SIGKILL chrome_crashpad_handler
 
-set +e
 ps aux |grep chrome
+set -e
