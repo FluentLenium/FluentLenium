2fac5b3a56070371b0ec8a6185d2e74d9931a455
Alphonse Bendt <alphonse.bendt@gmail.com>
2023-04-17 19:18:53 +0200

    debug

--- cleanup-browser.sh
+++ cleanup-browser.sh
@@ -2,15 +2,15 @@
 
 set +e
 echo "killing orphan chrome* processes with fire"
-killall -qv -s SIGKILL chromedriver
-killall -qv -s SIGKILL google-chrome
-killall -qv -s SIGKILL chrome
-killall -qv -s SIGKILL chrome_crashpad_handler
+killall -v -s SIGKILL chromedriver
+killall -v -s SIGKILL google-chrome
+killall -v -s SIGKILL chrome
+killall -v -s SIGKILL chrome_crashpad_handler
 
 echo "chrome processes"
 ps aux |grep chrome
 
 echo "open file handle count"
-lsof
 set -e
+lsof|wc -l
 
