fdede1b456833d9d400241720eab70a32dc6e4cc
Alphonse Bendt <370821+abendt@users.noreply.github.com>
2023-04-22 00:41:10 +0200

    fluentlenium-kotest: update kotest version (#1864)
    * update kotest version

--- cleanup-browser.sh
+++ cleanup-browser.sh
@@ -1,12 +1,16 @@
-#!/bin/bash -x
-
-echo "killing orphan chrome* processes with fire"
+#!/bin/bash
 
 set +e
-killall -qv -s SIGKILL chromedriver
-killall -qv -s SIGKILL google-chrome
-killall -qv -s SIGKILL chrome
-killall -qv -s SIGKILL chrome_crashpad_handler
+echo "killing orphan chrome* processes with fire"
+killall -v -s SIGKILL chromedriver
+killall -v -s SIGKILL google-chrome
+killall -v -s SIGKILL chrome
+killall -v -s SIGKILL chrome_crashpad_handler
 
+echo "chrome processes"
 ps aux |grep chrome
+
+echo "open file handle count"
 set -e
+lsof|wc -l
+
