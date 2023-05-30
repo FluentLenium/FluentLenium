52a76304b43df1db1da61afc861472f7e7488e65
Alphonse Bendt <alphonse.bendt@gmail.com>
2023-05-30 23:09:00 +0200

    add wait condition

--- examples/quickstart-safari/src/main/java/io/fluentlenium/examples/pages/DuckDuckMainPage.java
+++ examples/quickstart-safari/src/main/java/io/fluentlenium/examples/pages/DuckDuckMainPage.java
@@ -1,13 +1,13 @@
 package io.fluentlenium.examples.pages;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-import java.util.concurrent.TimeUnit;
-
 import io.fluentlenium.core.FluentPage;
 import io.fluentlenium.core.annotation.PageUrl;
 import io.fluentlenium.core.domain.FluentWebElement;
 import org.openqa.selenium.support.FindBy;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @PageUrl("https://duckduckgo.com")
 public class DuckDuckMainPage extends FluentPage {
@@ -20,6 +20,7 @@ public class DuckDuckMainPage extends FluentPage {
     private FluentWebElement searchButton;
 
     public DuckDuckMainPage typeSearchPhraseIn(String searchPhrase) {
+        await().until(searchInput).enabled();
         searchInput.write(searchPhrase);
         return this;
     }
