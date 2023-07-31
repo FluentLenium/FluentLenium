ae9b5a0322a7bd7ced2dae119a8829698b177672
Alphonse Bendt <alphonse.bendt@gmail.com>
2023-07-31 16:25:18 +0200

    refactor build scan publishing

--- .mvn/gradle-enterprise-custom-user-data.groovy
+++ .mvn/gradle-enterprise-custom-user-data.groovy
@@ -1,8 +1,24 @@
-buildScan.buildScanPublished {
+def isCI = System.getenv("CI") as Boolean
 
-    def scan = it.getBuildScanUri().toString()
-    def out = new File(".gradle_build_scan")
-    out.write("${scan}\n")
+def GITHUB_STEP_SUMMARY = System.getenv("GITHUB_STEP_SUMMARY")
 
-    println "wrote buildscan URL $scan to $out"
+println "gradle custom user data: $isCI"
+println GITHUB_STEP_SUMMARY
+
+if (isCI) {
+    buildScan.buildScanPublished {
+
+        def buildScan = it.getBuildScanUri().toString()
+
+        def badgeUrl="https://img.shields.io/badge/Build%20Scan%E2%84%A2-PUBLISHED-06A0CE?logo=Gradle"
+        def badgeHtml = """<img src="${badgeUrl}" alt="Build Scan Published" />"""
+        def url = """<a href="${buildScan}" rel="nofollow">${badgeHtml}</a>"""
+
+        def file = new File(GITHUB_STEP_SUMMARY)
+
+        println file
+
+        file.append(url)
+        println "appended $url to $file"
+    }
 }
