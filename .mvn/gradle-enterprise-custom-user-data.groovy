cbbba047497d6cb9404c89e4f5e008704a4c43fb
Alphonse Bendt <alphonse.bendt@gmail.com>
2023-08-02 14:23:21 +0200

    cleanup

--- .mvn/gradle-enterprise-custom-user-data.groovy
+++ .mvn/gradle-enterprise-custom-user-data.groovy
@@ -2,12 +2,8 @@ def isCI = System.getenv("CI") as Boolean
 
 def GITHUB_STEP_SUMMARY = System.getenv("GITHUB_STEP_SUMMARY")
 
-println "[INFO] gradle custom user data: $isCI"
-println "[INFO] GITHUB_STEP_SUMMARY=$GITHUB_STEP_SUMMARY"
-
 if (isCI) {
     buildScan.buildScanPublished {
-
         def buildScan = it.getBuildScanUri().toString()
 
         def badgeUrl="https://img.shields.io/badge/Build%20Scan%E2%84%A2-PUBLISHED-06A0CE?logo=Gradle"
@@ -15,8 +11,6 @@ if (isCI) {
         def url = """<a href="${buildScan}" rel="nofollow">${badgeHtml}</a>"""
 
         def file = new File(GITHUB_STEP_SUMMARY)
-
         file.append(url)
-        println "[INFO] appended $url to $file"
     }
 }
