1fa24f81526bd1bb0fb6721bd96bf7c3c9d52491
Alphonse Bendt <alphonse.bendt@gmail.com>
2024-03-02 20:42:51 +0100

    wip

--- mvnw.cmd
+++ mvnw.cmd
@@ -33,7 +33,7 @@
 @REM ----------------------------------------------------------------------------
 
 @REM Begin all REM lines with '@' in case MAVEN_BATCH_ECHO is 'on'
-@echo on
+@echo off
 @REM set title of command window
 title %0
 @REM enable echoing by setting MAVEN_BATCH_ECHO to 'on'
